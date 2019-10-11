5c8f5a28a5f713090f291e4a9420ee4fd718ef36
Ben <thisbenrogers@gmail.com>
2019-10-10 22:37:47 -0500

    :construction: Adds table with migrations for ingredients, recipe_ingredients, and steps
    creates .db3 files for each

---
+++ data/migrations/20191010221501_recipe_ingredients.js
@@ -0,0 +1,38 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('recipe_ingredients', recipe_ingredients => {
+
+    recipe_ingredients.increments();
+    
+    recipe_ingredients
+      .integer('recipe_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('recipes')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+
+      recipe_ingredients
+        .integer('ingredient_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('ingredients')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+
+
+    recipe_ingredients
+      .string('measure', 255)
+      .notNullable();
+
+    recipe_ingredients
+      .float('ingredients')
+  
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('recipe_ingredients');
+};
